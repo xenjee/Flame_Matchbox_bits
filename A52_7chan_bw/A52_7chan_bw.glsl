7151af93f71e7ba977aa8d0e5a5faa7f205c51c8
Stefan <xenjee@gmail.com>
2017-02-14 10:31:49 -0800

    Update A52_7chan_bw.glsl

--- A52_7chan_bw/A52_7chan_bw.glsl
+++ A52_7chan_bw/A52_7chan_bw.glsl
@@ -1,77 +1,90 @@
-\uniform float adsk_result_w, adsk_result_h;
+//Autodesk Variables
+uniform float adsk_result_w, adsk_result_h;
 vec2 resolution = vec2(adsk_result_w, adsk_result_h);
 
 uniform sampler2D Source;
-uniform float Exposure;
-uniform float amount;
+uniform float amount; // Affects the amount of blending between the original image and the desaturated one.
 
-// start of prog
+// Color Sliders
 uniform float Red;
 uniform float Green;
 uniform float Blue;
-
-// for adding cmyk > will need to be added to the xml (UI) - sg
 uniform float Cyan;
 uniform float Magenta;
 uniform float Yellow;
+uniform float Exposure;
 
-vec3 rgb2hsv(vec3 c)
-{
-    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
-
-    float d = q.x - min(q.w, q.y);
-    float e = 1.0e-10;
-    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+vec3 hsl2rgb( in vec3 c ){
+    vec3 rgb = clamp( abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0,1.0);
+    return c.z + c.y * (rgb-0.5)*(1.0-abs(2.0*c.z-1.0));
 }
 
-vec3 hsv2rgb(vec3 c)
+vec3 rgb2hsl( in vec3 c )
 {
-    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+    const float epsilon = 0.00000001;
+    float cmin = min( c.r, min( c.g, c.b ) );
+    float cmax = max( c.r, max( c.g, c.b ) );
+    float cd   = cmax - cmin;
+    vec3 hsl = vec3(0.0);
+    hsl.z = (cmax + cmin) / 2.0;
+    hsl.y = mix(cd / (cmax + cmin + epsilon), cd / (epsilon + 2.0 - (cmax + cmin)), step(0.5, hsl.z));
+    
+    // Special handling for the case of 2 components being equal and max at the same time,
+    // this can probably be improved but it is a nice proof of concept
+    vec3 a = vec3(1.0 - step(epsilon, abs(cmax - c)));
+    a = mix(vec3(a.x, 0.0, a.z), a, step(0.5, 2.0 - a.x - a.y));
+    a = mix(vec3(a.x, a.y, 0.0), a, step(0.5, 2.0 - a.x - a.z));
+    a = mix(vec3(a.x, a.y, 0.0), a, step(0.5, 2.0 - a.y - a.z));
+    
+    hsl.x = dot( vec3(0.0, 2.0, 4.0) + ((c.gbr - c.brg) / (epsilon + cd)), a );
+    hsl.x = (hsl.x + (1.0 - step(0.0, hsl.x) ) * 6.0 ) / 6.0;
+    return hsl;
+}
+
+float blendColorEffect(in float color1, in float color2, in float percentage)
+{
+    return color1 + percentage*(color2-color1);
 }
 
 
-void main (void) // need to add the cmyk part - sg
-{ 		
-	vec2 uv = gl_FragCoord.xy / resolution.xy;
-	vec4 tc = texture2D(Source, uv);
-	vec3 fragRGB = tc.rgb;
-	vec3 fragHSV = rgb2hsv(fragRGB.xyz);
+void main (void)
+{
+    vec2 uv = gl_FragCoord.xy / resolution.xy;
+    vec4 tc = texture2D(Source, uv);
+    vec3 fragRGB = tc.rgb;
+	vec3 fragHSL = rgb2hsl(fragRGB.xyz);
 	
-	if (fragHSV.x <= 30) // First half of RED
+	if (fragHSL.x <= 30.0/360.0) // First half of RED
 	{
-		fragHSV.y -= Red;
+        fragHSL.y -= blendColorEffect(Red, (Red+Yellow)/2.0, fragHSL.x/(30.0/360.0));
 	}
-	else if (fragHSV.x <= 90) // YELLOW
+    else if (fragHSL.x <= 90.0/360.0) // YELLOW
 	{
-		fragHSV.y -= Yellow;
+		fragHSL.y -= blendColorEffect((Red+Yellow)/2.0, (Yellow+Green)/2.0, (fragHSL.x-(30.0/360.0))/(60.0/360.0));
 	}
-	else if (fragHSV.x <= 150) // GREEN
+	else if (fragHSL.x <= 150.0/360.0) // GREEN
 	{
-		fragHSV.y -= Green;
+		fragHSL.y -= blendColorEffect((Yellow+Green)/2.0, (Green+Cyan)/2.0, (fragHSL.x-(90.0/360.0))/(60.0/360.0));
 	}
-	else if (fragHSV.x <= 210) // CYAN
+	else if (fragHSL.x <= 210.0/360.0) // CYAN
 	{
-		fragHSV.y -= Cyan;
+		fragHSL.y -= blendColorEffect((Green+Cyan)/2.0, (Cyan+Blue)/2.0, (fragHSL.x-(150.0/360.0))/(60.0/360.0));
 	}
-	else if (fragHSV.x <= 270) // BLUE
+	else if (fragHSL.x <= 270.0/360.0) // BLUE
 	{
-		fragHSV.y -= Blue;
+		fragHSL.y -= blendColorEffect((Cyan+Blue)/2.0, (Blue+Magenta)/2.0, (fragHSL.x-(210.0/360.0))/(60.0/360.0));
 	}
-	else if (fragHSV.x <= 330) // MAGENTA
+	else if (fragHSL.x <= 330.0/360.0) // MAGENTA
 	{
-		fragHSV.y -= Magenta;
+		fragHSL.y -= blendColorEffect((Blue+Magenta)/2.0, (Magenta+Red)/2.0, (fragHSL.x-(270.0/360.0))/(60.0/360.0));
 	}
 	else // Last half of RED
 	{
-		fragHSV.y -= RED;
+		fragHSL.y -= blendColorEffect((Magenta+Red)/2.0, Red, (fragHSL.x-(330.0/360.0))/(30.0/360.0));
 	}
-	
-	//fragHSV.xyz = min(max(0.0, fragHSV.xyz), 1.0);
-	fragRGB = hsv2rgb(fragHSV);
-
-	gl_FragColor = vec4(fragRGB, amount);//mix(tc.rgb, fragRGB, amount);
+    
+    //fragHSV = min(max(vec3(0.0,0.0,0.0), fragHSL.xyz), vec3(1.0, 1.0, 1.0));
+    fragRGB = hsl2rgb(fragHSL);
+    
+    gl_FragColor = mix(tc, vec4(fragRGB, 1.0), amount);
 }
