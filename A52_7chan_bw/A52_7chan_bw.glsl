383f3b11c06c24e7657c4aa00f4002fc37acac4f
Stefan <xenjee@gmail.com>
2017-02-13 18:16:44 -0800

    Update A52_7chan_bw.glsl

--- A52_7chan_bw/A52_7chan_bw.glsl
+++ A52_7chan_bw/A52_7chan_bw.glsl
@@ -1,3 +1,6 @@
+// rev - 20170213
+
+
 uniform float adsk_result_w, adsk_result_h;
 vec2 resolution = vec2(adsk_result_w, adsk_result_h);
 
@@ -11,23 +14,25 @@ uniform float Red;
 uniform float Green;
 uniform float Blue;
 
-// adding cmyk > to be added in UI xml as well
+// adding cmyk > added in UI xml too
 uniform float Cyan;
 uniform float Magenta;
 uniform float Yellow;
 //uniform float Black; // Unneccessary. You can calculate black from CMY - tf
 
 // Convert the CMY values to RGB
-float Black = min(Cyan, Magenta, Yellow); 
+float BlackA = min(Cyan, Magenta);
+float Black = min(BlackA, Yellow); 
 float newCyan = Cyan - Black;
 float newMagenta = Magenta - Black;
 float newYellow = Yellow - Black;
 
-float newRed = Red + (1 - Cyan) * (1 - Black);
-float newGreen = Green + (1 - Magenta) * (1 - Black)
-float newBlue = Blue + (1 - Yellow) * (1 - Black)
+float newRed = Red + (1 - newCyan) * (1 - Black);
+float newGreen = Green + (1 - newMagenta) * (1 - Black);
+float newBlue = Blue + (1 - newYellow) * (1 - Black);
 
 
+//vec3 RGB_lum = vec3(Red, Green, Blue); 
 vec3 RGB_lum = vec3(newRed, newGreen, newBlue);
 const vec3 lumcoeff = vec3(0.2126,0.7152,0.0722);
 
